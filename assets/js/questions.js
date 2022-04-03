7c9113f722ae649e6ebaa6e4bfbc30b157877cd4
AshLooker <Ash.looker14@gmail.com>
2022-04-03 15:03:17 +0000

    added result area

---
+++ assets/js/questions.js
@@ -0,0 +1,222 @@
+let questions = [
+    {
+        Number: 1,
+        question: "Who invented JavaScript?",
+        answer: "Brendan Eich",
+        options: [
+            "Douglas Crockford",
+            "Sheryl Sandberg",
+            "Brendan Eich",
+            "Steve Wozniak"
+        ]
+    },
+    {
+        Number: 2,
+        question: "What was the first computer that had a colour display?",
+        answer: "Apple II",
+        options: [
+            "iMac",
+            "Apple II",
+            "BBC Micro",
+            "Amiga 500"
+        ]
+    },
+    {
+        Number: 3,
+        question: "What percentage of people are said to plug their USB devices incorrectly during their first attempt?",
+        answer: "86%",
+        options: [
+            "86%",
+            "Must be 100%",
+            "41%",
+            "I assume eveyone is just like me, so 0%"
+        ]
+    },
+    {
+        Number: 4,
+        question: "Which company first developed the Java programming language?",
+        answer: "Sun Microsystems",
+        options: [
+            "Microsoft",
+            "Google",
+            "Sun Microsystems",
+            "IBM"
+        ]
+    },
+    {
+        Number: 5,
+        question: "What do you call a computer's main circuit board?",
+        answer: "Motherboard",
+        options: [
+            "Computer board",
+            "Micro Board",
+            "Motherboard",
+            "Fatherboard"
+        ]
+    },
+    {
+        Number: 6,
+        question: "What does I.T. stand for?",
+        answer: "Information Technology",
+        options: [
+            "Internet Technology",
+            "Information Technology",
+            "Intranet Technology",
+            "Important Technology"
+        ]
+    },
+    {
+        Number: 7,
+        question: "Who created the first modern computer?",
+        answer: "Charles Babbage",
+        options: [
+            "Steve Jobs",
+            "Bill Gates",
+            "Arthur C Clarke",
+            "Charles Babbage"
+        ]
+    },
+    {
+        Number: 8,
+        question: "Which one is the first search engine in internet?",
+        answer: "Archie",
+        options: [
+            "Google",
+            "Archie",
+            "Altavista",
+            "WAIS"
+        ]
+    },
+    {
+        Number: 9,
+        question: "Which one is the first web browser, invented in 1990?",
+        answer: "Nexus",
+        options: [
+            "Internet Explorer",
+            "Mosaic",
+            "Mozilla",
+            "Nexus"
+        ]
+    },
+    {
+        Number: 10,
+        question: "Which of the folowing programming languages is used to create programs like applets?",
+        answer: "Java",
+        options: [
+            "COBOL",
+            "C Langauge",
+            "Java",
+            "BASIC"
+        ]
+    },
+    {
+        Number: 11,
+        question: "What was the first computer virus known as?",
+        answer: "Creeper Virus",
+        options: [
+            "Rabbit",
+            "Creeper Virus",
+            "Elk CLoner",
+            "SCA Virus"
+        ]
+    },
+    {
+        Number: 12,
+        question: "Which one of these languages is exclusively used for artificial intelligence?",
+        answer: "Prolog",
+        options: [
+            "C",
+            "Java",
+            "J2EE",
+            "Prolog"
+        ]
+    },
+    {
+        Number: 13,
+        question: "Firewall in computer is used for what?",
+        answer: "Security",
+        options: [
+            "Security",
+            "Data Transmission",
+            "Authentication",
+            "Monitoring"
+        ]
+    },
+    {
+        Number: 14,
+        question: "Which of the following, is not an operating system?",
+        answer: "C",
+        options: [
+            "DOS",
+            "Mac",
+            "C",
+            "Linux"
+        ]
+    },
+    {
+        Number: 15,
+        question: "How many layers are in the OSI (Open Systems Interconnection) Model?",
+        answer: "7",
+        options: [
+            "9",
+            "3",
+            "7",
+            "11"
+        ]
+    },
+    {
+        Number: 16,
+        question: "Where is the headquarters of Microsoft located?",
+        answer: "Washington",
+        options: [
+            "Texas",
+            "New York",
+            "California",
+            "Washington"
+        ]
+    },
+    {
+        Number: 17,
+        question: "Computer Hard Disks were first introduced in 1956 by which company?",
+        answer: "IBM",
+        options: [
+            "Dell",
+            "Microsoft",
+            "Apple",
+            "IBM"
+        ]
+    },
+    {
+        Number: 18,
+        question: "A computer uses which type of number system to calculate and store data?",
+        answer: "Binary",
+        options: [
+            "Decimal",
+            "Octal",
+            "Binary",
+            "Hexadecimal"
+        ]
+    },
+    {
+        Number: 19,
+        question: "In the computer world, a Trojan is referred to, as what?",
+        answer: "Malware",
+        options: [
+            "Virus",
+            "Malware",
+            "Worm",
+            "Spyware"
+        ]
+    },
+    {
+        Number: 20,
+        question: "How many layers are in the TCP/IP protocol?",
+        answer: "4",
+        options: [
+            "7",
+            "5",
+            "4",
+            "11"
+        ]
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ const rule_box = document.querySelector(".rule_box");
 const quit_btn = rule_box.querySelector(".buttons .quit"); 
 const continue_btn = rule_box.querySelector(".buttons .restart");
 const quiz_area = document.querySelector(".quiz_area");
+const timeCounter = quiz_area.querySelector(".timer .timer_sec");
+const timeLine = quiz_area.querySelector("header .time_line");
+
+const answer_list = document.querySelector(".answer_list");
+
 
 start_btn.onclick = () => {
     rule_box.classList.add("activeRule");
@@ -15,4 +20,174 @@ quit_btn.onclick = () => {
 continue_btn.onclick = () => {
     rule_box.classList.remove("activeRule");
     quiz_area.classList.add("activeQuiz");
+    showQuestions(0);
+    questionCounter(1);
+    startTimer(15);
+    startTimerLine(0);
+}
+
+let question_count = 0;
+let question_number = 1;
+let counter;
+let counterLine;
+let timeValue = 15;
+let widthValue = 0;
+let userScore = 0;
+
+const next_btn = quiz_area.querySelector(".next_btn");
+const result_area = document.querySelector(".result_area");
+const restart_quiz = result_area.querySelector(".buttons .restart");
+const quit = result_area.querySelector(".buttons .quit");
+
+restart_quiz.onclick = () =>{
+    quiz_area.classList.add(".activeQuiz"); 
+    result_area.classList.remove(".activeResult");
+    let question_count = 0;
+    let question_number = 1;
+    let timeValue = 15;
+    let widthValue = 0;
+    let userScore = 0;
+    showQuestions(question_count);
+    questionCounter(question_number);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimer(widthValue);
+    next_btn.style.display = "none";
+   
+}
+
+quit.onclick = () =>{
+    window.location.reload();
+}
+
+next_btn.onclick = ()=> {
+    if(question_count < questions.length - 1) {
+        question_count++;
+        question_number++;
+        showQuestions(question_count);
+        questionCounter(question_number);
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimer(widthValue);
+        next_btn.style.display = "none";
+    }else{
+        console.log("Questions Completed");
+        showResultArea();
+    }
+}
+
+function showQuestions(index){
+    const question = document.querySelector(".question");
+    let question_tag = '<span>'+ questions[index].Number + ". " + questions[index].question +'<span>';
+    let answer_tag = '<div class="options">'+ questions[index].options[0] +'<span></span></div>'
+                     + '<div class="options">'+ questions[index].options[1] +'<span></span></div>'
+                     + '<div class="options">'+ questions[index].options[2] +'<span></span></div>'
+                     + '<div class="options">'+ questions[index].options[3] +'<span></span></div>';
+    question.innerHTML = question_tag;
+    answer_list.innerHTML = answer_tag;
+    
+    const options = answer_list.querySelectorAll(".options");
+    for(let i = 0; i < options.length; i++) {
+        options[i].setAttribute("onclick","optionSelected(this)");
+    }
+}
+
+
+
+let tickIcon = '<div class="icon tick"><i class="fa-solid fa-check"></i></div>'; 
+let crossIcon = '<div class="icon cross"><i class="fa-solid fa-xmark"></i></div>'; 
+
+
+function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[question_count].answer;
+    let allOptions = answer_list.children.length;
+    if(userAnswer == correctAnswer){
+        userScore += 1;
+        console.log(userScore);
+        answer.classList.add("correct")
+        console.log("Answer is Correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    }else{
+        answer.classList.add("incorrect");
+        console.log("Answer is Incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+
+        
+        for(let i = 0; i < allOptions; i++) {
+            if(answer_list.children[i].textContent == correctAnswer){
+                answer_list.children[i].setAttribute("class","options correct");
+                answer_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+            }
+        }
+    }
+
+    for (let i = 0; i < allOptions; i++) {
+        answer_list.children[i].classList.add("disabled");
+    }
+    next_btn.style.display = "block";
+}
+
+function showResultArea() {
+    rule_box.classList.remove("activeRule");
+    quiz_area.classList.remove("activeQuiz");
+    result_area.classList.add("activeResult");
+    const score = result_area.querySelector(".score");
+    if (userScore > 19){
+        let scoreTag = '<span>Awesome!! You got <p>'+ userScore +'</p>out of <p>'+ questions.length +'</p>You are a Wizard</span>'
+        score.innerHTML = scoreTag;
+    }
+    else if (userScore > 10){
+        let scoreTag = '<span> Nice!! You got <p>'+ userScore +'</p>out of <p>'+ questions.length +'</p>Can you do better!?</span>'
+        score.innerHTML = scoreTag;
+    }
+    else{
+        let scoreTag = '<span>Sorry, you got <p>'+ userScore +'</p>out of <p>'+ questions.length +'</p>Have another try!</span>'
+        score.innerHTML = scoreTag;
+    }
+}
+
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCounter.textContent = time;
+        time--; 
+        if (time < 9){
+            let addZero = timeCounter.textContent;
+            timeCounter.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCounter.textContent = "00";
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 20);
+    function timer(){
+        time += 1;
+        timeLine.style.width = time + "px";
+        if (time > 799){
+            clearInterval(counterLine);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+function questionCounter (index){
+    const bottom_questions_counter = quiz_area.querySelector(".total_questions");
+    let totalQuestionsCounterTag = '<span><p>'+ question_count +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+    bottom_questions_counter.innerHTML = totalQuestionsCounterTag;
 }
